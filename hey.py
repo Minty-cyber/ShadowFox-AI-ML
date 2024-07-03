e5b2a1b8ce84741d0cc3d537f180877897b26152
Jmint <jeffreymintah737@gmail.com>
2024-07-03 20:41:11 +0000

    changes

--- hey.py
+++ hey.py
@@ -2,3 +2,9 @@ from detecto import core, utils
 from detecto.visualize import show_labeled_image
 from torchvision import transforms
 import numpy as np
+
+
+
+path_image = 'images'
+path_train_labels ='train_labels'
+path_test_labels ='test_labels'
