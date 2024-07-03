d8d1a6f0e715f35cf5ae7fd6d52395ae0eb21918
Jmint <jeffreymintah737@gmail.com>
2024-07-03 20:46:18 +0000

    changes

--- hey.py
+++ hey.py
@@ -13,6 +13,7 @@ path_test_labels ='test_labels'
 custom_transforms = transforms.Compose([
     transforms.ToPILImage(),#Converts it to pillow image
     transforms.Resize(50), #Resizes the images to a smaller size
-    transforms.RandomHorizontalFlip(),
+    transforms.RandomHorizontalFlip(), #Improves the nborder performance
+    
     
 ])
